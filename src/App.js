3098a63015cbeb5766e3b631475b07b7256a7fe6
unknown <tarekyosry8@gmail.com>
2023-08-24 02:41:50 +0300

    Create increment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import { UseSelector, useSelector } from 'react-redux/es/hooks/useSelector';
+import { useSelector, useDispatch } from 'react-redux'
 import './App.css';
 
 function App() {
   const counter = useSelector(state => state.counter)
+  const dispatch = useDispatch()
+  const increment = () => {
+    dispatch({type: 'INC'});
+  }
+  const decrement = () => {
+
+  }
   return (
     <div className="App">
       <h1>Counter App</h1>
       <h2>{counter}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
     </div>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,10 @@ import { legacy_createStore as createStore} from 'redux'
 
 
 const reducerFn = (state = { counter: 0 }, action) => {
+
+    if(action.type === 'INC'){
+        return {counter: state.counter + 1}
+    }
     return state;
 }
 
